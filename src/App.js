6f1fe1f437efe2a5738ca5c5292bb8b0f17a28f9
Akash-Raghuwanshi <akashraghuwanshi99@gmail.com>
2023-11-21 10:05:03 +0530

    added subheader and updated them

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import Header from "./Header";
 import Product from "./Product";
-
+import SubHeader from "./SubHeader";
 function App() {
   return (
     <div className="App">
       <Header/>
+      <SubHeader/>
       <Product/>
     </div>
   );
