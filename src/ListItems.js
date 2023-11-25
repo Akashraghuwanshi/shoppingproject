f184f60e5c6229106eb7b08e1d527c80c4396659
Akash-Raghuwanshi <akashraghuwanshi99@gmail.com>
2023-11-25 19:53:38 +0530

    added hook to ListItems.js and updated style.css by adding class cart-message

--- src/ListItems.js
+++ src/ListItems.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import AddToCardIcon from './AssetIcons/addtocard.webp'
+import { useState } from 'react'
 const ListItems = ({data}) => {
+
+  const[message,setMessage]=useState("Not added to the card yet")
+  
+  const handleClick = ()=>{
+    setMessage("Added to the card!");
+    console.log("clicked,",message);
+  }
   return (
     <div className='item-card'>
     <img className={'img-fluid'} src={`/Images/${data.thumnail}`} alt={data.title} />
@@ -15,7 +23,8 @@ const ListItems = ({data}) => {
          <a href="https://www.amazon.in" target="_blank" rel="noreferrer"><h3>{`${data.title}`}</h3> </a>
          </div>
   </div>
-  <button className={'card-add'}> Add to Card
+  <small className="cart-message">{message}</small>
+  <button className={'card-add'} onClick={handleClick}> Add to Card
   <img src={AddToCardIcon} alt="addtocard" width ="35px" />
   </button>
   </div>
